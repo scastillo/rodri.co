362d615db6b2f114b878f51d0c56f6b7ee099035
scastillo <castillobuiles@gmail.com>
2014-10-27 15:55:03 -0500

    Iniital commit

---
+++ app.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main(){
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/", fs)
+	log.Println("Listening...")
+	http.ListenAndServe(":9090", nil)
+}
